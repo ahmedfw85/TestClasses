11ff161544037b2690fae980df356670b868015b
Ahmad Wedyan <Ahmad.Wedyan@atcltd.atc.org.nz>
2021-03-12 11:25:49 +1300

    Add project files.

---
+++ TestClasses/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestClasses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //to creat an object 
+            // ClassName objectName = new ClassName ();
+
+            Person p = new Person();
+            label1.Text = p.ShowDetails();
+            label2.Text = p.AddFiveToAge().ToString();
+
+        }
+    }
+}
---
+++ TestClasses/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClasses
+{
+    class Person
+    {
+        String name;
+        int age;
+
+        // Constructor that takes no arguments:
+        public Person()
+        {
+            name = "unknown";
+            age = 18;
+        }
+
+        // Constructor that takes one argument:
+        public Person(string NewName, int NewAge)
+        {
+            name = NewName;
+            age = NewAge;
+        }
+        //--------------- Methods------------
+        public String ShowDetails()
+        {
+            return "Name: " + name + ", Age: " + age;
+        }
+        public int AddFiveToAge()
+        {
+            return age + 5;
+        }
+    } 
+}
